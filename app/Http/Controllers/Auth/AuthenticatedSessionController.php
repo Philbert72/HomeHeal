f85c38649109d0974a9349647b5f323536c702fa
Philbert72 <philberttan72@gmail.com>
2026-01-10 14:06:18 +0700

    add destroy method to authenticate controller

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -37,4 +37,18 @@ class AuthenticatedSessionController extends Controller
 
         return redirect()->intended(route('dashboard'));
     }
+
+    /**
+     * Destroy an authenticated session (Logout).
+     */
+    public function destroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
 }
